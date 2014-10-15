b927654cc72abb416f42ed23d583c3091e6f571f
Jason Puglisi <jason@puglisi.im>
2014-10-15 13:11:57 -0400

    Moved dance loop to another function

--- modules/dance.js
+++ modules/dance.js
@@ -5,24 +5,29 @@ module.exports = {
 		var limit = args[0];
 		var speed = args[1] || 1;
 
-		if (isNaN(limit) || limit < 1) limit = 1;
-		limit = Math.min(limit, 50);
+		if (isNaN(limit) || limit < 1)
+			limit = 1;
+		if (isNaN(speed) || speed < 1)
+			speed = 1;
 
-		if (isNaN(speed) || speed < 1) speed = 1;
+		limit = Math.min(limit, 50);
 		speed = Math.min(speed, 5);
 
 		for (var i = 1; i <= limit; i++)
 			setTimeout(function() {
 				danceCount++;
-
-				if (limit === danceCount)
-					rpc.emit('call', client, 'privmsg', [target, '\\o/']);
-
-				else if (danceCount % 2 === 1)
-					rpc.emit('call', client, 'privmsg', [target, '/o/']);
-
-				else
-					rpc.emit('call', client, 'privmsg', [target, '\\o\\']);
+				sendDance(client, target, danceCount, limit);
 			}, (1500 / speed) * i);
+	},
+
+	sendDance: function(client, target, danceCount, limit) {
+		if (limit === danceCount)
+			rpc.emit('call', client, 'privmsg', [target, '\\o/']);
+
+		else if (danceCount % 2 === 1)
+			rpc.emit('call', client, 'privmsg', [target, '/o/']);
+
+		else
+			rpc.emit('call', client, 'privmsg', [target, '\\o\\']);
 	}
 };

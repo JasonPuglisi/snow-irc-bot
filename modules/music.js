36f3b3709d957cc51f2eb4ed648f21008886b911
Jason Puglisi <jason@puglisi.im>
2014-10-21 15:22:17 -0400

    Limited results to one track for efficiency and removed url from output

--- modules/music.js
+++ modules/music.js
@@ -14,19 +14,19 @@ module.exports = {
 
 			if (input !== undefined) {
 				var user = iconv.decode(new Buffer(input), 'ISO-8859-1');
-				var playingUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + lastfmKey + '&format=json';
+				var playingUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + lastfmKey + '&limit=1&format=json';
 
 				request(playingUrl, function (error, response, body) {
 					if (!error && response.statusCode === 200) {
 						var data = JSON.parse(body);
 
 						if (data.recenttracks !== undefined) {
-							if (data.recenttracks.track !== undefined && data.recenttracks.track[0]['@attr']) {
+							if (data.recenttracks.track && data.recenttracks.track[0] && data.recenttracks.track[0]['@attr']) {
 								var track = data.recenttracks.track[0].name;
 								var artist = data.recenttracks.track[0].artist['#text'];
 								var url = data.recenttracks.track[0].url;
 
-								rpc.emit('call', client, 'privmsg', [target, prefix + user + ' is playing \'' + track + '\' by ' + artist + ' • ' + url]);
+								rpc.emit('call', client, 'privmsg', [target, prefix + user + ' is playing \'' + track + '\' by ' + artist]);
 							}
 
 							else

9f1cefd03ae084b26762a45d841fc42c16ec6ef8
Jason Puglisi <jason@puglisi.im>
2014-10-22 14:56:31 -0400

    Refractored 'playing' to 'listening' in all instances to avoid possible future conflict

--- modules/music.js
+++ modules/music.js
@@ -1,5 +1,5 @@
 module.exports = {
-	getLastfmPlaying: function(client, network, channel, command, trigger, nickname, target, args, prefix) {
+	getLastfmListening: function(client, network, channel, command, trigger, nickname, target, args, prefix) {
 		if (config.apis.lastfm !== undefined)
 			var lastfmKey = config.apis.lastfm.key;
 
@@ -14,23 +14,25 @@ module.exports = {
 
 			if (input !== undefined) {
 				var user = iconv.decode(new Buffer(input), 'ISO-8859-1');
-				var playingUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + lastfmKey + '&limit=1&format=json';
+				var listeningUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + lastfmKey + '&limit=1&format=json';
 
-				request(playingUrl, function (error, response, body) {
+				request(listeningUrl, function (error, response, body) {
 					if (!error && response.statusCode === 200) {
 						var data = JSON.parse(body);
 
 						if (data.recenttracks !== undefined) {
 							if (data.recenttracks.track && data.recenttracks.track[0] && data.recenttracks.track[0]['@attr']) {
+								user = data.recenttracks['@attr'].user;
+
 								var track = data.recenttracks.track[0].name;
 								var artist = data.recenttracks.track[0].artist['#text'];
 								var url = data.recenttracks.track[0].url;
 
-								rpc.emit('call', client, 'privmsg', [target, prefix + user + ' is playing \'' + track + '\' by ' + artist]);
+								rpc.emit('call', client, 'privmsg', [target, prefix + user + ' is listening to \'' + track + '\' by ' + artist]);
 							}
 
 							else
-								rpc.emit('call', client, 'privmsg', [target, prefix + 'The user \'' + user + '\' is not playing anything']);
+								rpc.emit('call', client, 'privmsg', [target, prefix + user + ' is not listening to anything']);
 						}
 
 
